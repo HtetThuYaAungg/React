6973574119ff9bcb5c6f68f61cdd3e34397d95c6
HtetThuYaAungg <htetthuya.htya@gmail.com>
2023-01-22 17:23:18 +0630

    initial

---
+++ react-invoice/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./App.scss";
+import Invoice from './components/Invoice';
+import Nav from './components/Nav';
+
+const App = () => {
+
+  return (
+   
+     <>
+      
+      
+      <Invoice/>
+     </>
+
+  )
+}
+
+export default App
