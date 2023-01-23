1e0e043e7f31a718790e4c0845c6ac0d2c4dfcba
HtetThuYaAungg <htetthuya.htya@gmail.com>
2023-01-23 09:17:35 +0630

    initial

---
+++ react-shop/src/components/Shop.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import NavBar from './NavBar'
+
+const Shop = () => {
+  return <div className="">
+    <div className="container">
+        <div className="row">
+            <div className="col-12">
+                <div className="d-flex justify-content-between align-items-center border rounded border-dark p-3">
+                    <h3>ONLINE SHOP</h3>
+                    <div className="">
+                        <button className="btn btn-outline-dark me-2">
+                            <i className="bi bi-search"></i>
+                        </button>
+                        <button className="btn btn-outline-dark">
+                            <i className="bi bi-bag"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+}
+
+export default Shop
+
